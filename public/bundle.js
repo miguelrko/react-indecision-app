b934faca486ad001fd7e2d62a9115286f9a7f98d
Miguel Anez <MZwK@MacBook-Pro.local>
2018-01-23 13:59:26 -0600

    Implementación de webpack basico

---
+++ public/bundle.js
@@ -0,0 +1,131 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person_js__ = __webpack_require__(2);
+
+
+//para los default:
+//import defaultnamethatyouwant, {square, add} from './utils.js';
+
+console.log('webpack configuration test');
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* square */](5));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](50, 50));
+
+//Challenge: create person.js
+//named export isAdult(18) - true if adult, otherwise false
+//named export canDrink(18) - true if 21 and over, otherwise false
+//import isAdult and canDrin
+//use both printing result to the console
+console.log('I am an adult ', __WEBPACK_IMPORTED_MODULE_1__person_js__["c" /* isAdult */](19));
+console.log('Can i drink? ', __WEBPACK_IMPORTED_MODULE_1__person_js__["a" /* canDrink */](19));
+console.log('Am i too old? ', __WEBPACK_IMPORTED_MODULE_1__person_js__["b" /* default */](61));
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
+//test file for exports
+console.log('utils file was loaded');
+
+const square = ((x) => x * x);
+
+// export const add = (a, b) => a + b;
+const add = (a ,b) => a + b;
+
+//named exports
+
+
+//default exports
+//los default te permiten llamarlos con el nombre que desees al importarlos
+//export { square, add as default};
+//export default add;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = ((x) => x >= 18);
+const canDrink = ((x) => x >= 21);
+
+/* harmony default export */ __webpack_exports__["b"] = ((x) => x >= 60);
+
+
+
+/***/ })
+/******/ ]);
---
+++ src/person.js
@@ -0,0 +1,6 @@
+const isAdult = ((x) => x >= 18);
+const canDrink = ((x) => x >= 21);
+
+export default (x) => x >= 60;
+
+export { isAdult, canDrink };
